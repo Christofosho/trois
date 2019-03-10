2f68d578fb705348827e184209c56175cec60c13
Christopher Snow <csnow@live.ca>
2019-03-10 00:11:27 -0500

    Tweak the style and add instructions.

--- public/js/index.js
+++ public/js/index.js
@@ -27,4 +27,23 @@ socket.addEventListener('close', (event) => {
 
 });
 
+
+let overlay = document.querySelector('.overlay');
+let close = document.querySelector('.close');
+let instructions_toggle = document.querySelector('.instructions-toggle');
+let overlay_toggle = (e) => {
+    e.preventDefault();
+    overlay.classList.toggle('hide');
+};
+overlay.addEventListener('click', overlay_toggle);
+close.addEventListener('click', overlay_toggle);
+instructions_toggle.addEventListener('click', overlay_toggle);
+
+let instructions = document.querySelector('.instructions');
+let overlay_ignore = (e) => {
+    e.stopPropagation();
+};
+instructions.addEventListener('click', overlay_ignore);
+
+
 export {socket};
